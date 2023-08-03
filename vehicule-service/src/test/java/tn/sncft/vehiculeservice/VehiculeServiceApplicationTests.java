b8a241602481a888c99738a9e513aaa81a9a27a8
mouhamed92 <hamamimohamed92@gmail.com>
2023-08-03 08:22:08 +0200

    véhicule-service 1.0

---
+++ vehicule-service/src/test/java/tn/sncft/vehiculeservice/VehiculeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.sncft.vehiculeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
